71a55d036206e2e62edfe4019fbab3fd557ccdd2
Hasnat Jamil <hasnatjamil2018@gmail.com>
2022-11-27 23:53:50 +0600

    changeset: setup API core

---
+++ src/core/apiCore.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const sendGetRequest = async (
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse<any, any>> => {
+  try {
+    const res = await axios.get(url, config)
+    return res
+  } catch (error) {
+    if (axios.isAxiosError(error)) throw new Error(error.message)
+    throw new Error('An unexpected error occurred.')
+  }
+}
+
+const sendPostRequest = async (
+  url: string,
+  payload: object,
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse<any, any>> => {
+  try {
+    const res = await axios.post(url, payload, config)
+    return res
+  } catch (error) {
+    if (axios.isAxiosError(error)) throw new Error(error.message)
+    throw new Error('An unexpected error occurred.')
+  }
+}
+
+const sendPutRequest = async (
+  url: string,
+  payload: object,
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse<any, any>> => {
+  try {
+    const res = await axios.put(url, payload, config)
+    return res
+  } catch (error) {
+    if (axios.isAxiosError(error)) throw new Error(error.message)
+    throw new Error('An unexpected error occurred.')
+  }
+}
+
+const sendDeleteRequest = async (
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse<any, any>> => {
+  try {
+    const res = await axios.delete(url, config)
+    return res
+  } catch (error) {
+    if (axios.isAxiosError(error)) throw new Error(error.message)
+    throw new Error('An unexpected error occurred.')
+  }
+}
+
+export { sendGetRequest, sendPostRequest, sendPutRequest, sendDeleteRequest }
