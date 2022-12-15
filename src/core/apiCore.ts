74777e18a287f701ae2b42a54437dabba0531558
jamil2018 <hasnatjamil2018@gmail.com>
2022-12-16 02:50:49 +0600

    changeset: implement api core using native fetch api

--- src/core/apiCore.ts
+++ src/core/apiCore.ts
@@ -1,3 +1,5 @@
+import { getErrorMessage } from '../utils'
+
 const sendGETRequest = async (
   url: string,
   config?: RequestInit
@@ -7,8 +9,76 @@ const sendGETRequest = async (
     const data = await response.json()
     return data
   } catch (error) {
-    throw new Error('Error occurred while fetching data')
+    throw new Error(
+      `Error occurred while fetching data. Request type: GET. Request url: ${url}. Error: ${getErrorMessage(
+        error
+      )}`
+    )
   }
 }
 
-export { sendGETRequest }
+const sendPOSTRequest = async (
+  url: string,
+  body: object,
+  config?: RequestInit
+): Promise<any> => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      ...config,
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(
+      `Error occurred while fetching data. Request type: POST. Request url: ${url}. Error: ${getErrorMessage(
+        error
+      )}`
+    )
+  }
+}
+
+const sendPUTRequest = async (
+  url: string,
+  body: object,
+  config?: RequestInit
+): Promise<any> => {
+  try {
+    const response = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(body),
+      ...config,
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(
+      `Error occurred while fetching data. Request type: PUT. Request url: ${url}. Error: ${getErrorMessage(
+        error
+      )}`
+    )
+  }
+}
+
+const sendDELETERequest = async (
+  url: string,
+  config?: RequestInit
+): Promise<any> => {
+  try {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      ...config,
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(
+      `Error occurred while fetching data. Request type: DELETE. Request url: ${url}. Error: ${getErrorMessage(
+        error
+      )}`
+    )
+  }
+}
+
+export { sendGETRequest, sendPOSTRequest, sendPUTRequest, sendDELETERequest }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { getAllTestFiles } from './pathUtils'
 export { getReporterConfig } from './configUtils'
+export { getErrorMessage } from './codeUtils'
